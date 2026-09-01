f358b65cc9e371fa7810d3e6e53f8b1ba481b4bd
Zaid Mehmood <kzaid2918@gmail.com>
2026-09-01 12:42:48 +0500

    hardcoded'

--- backend/api/config.js
+++ backend/api/config.js
@@ -1,2 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-console.log('🔍 API_URL is:', API_URL);
+export const API_URL = 'https://shop-co-backend-azure.vercel.app/api';
